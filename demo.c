41a466a77bba1dce56476dbff2f8cc6176ecceac
Jacek Wieczorek <mrjjot@gmail.com>
2019-05-05 19:25:57 +0200

    Improve demo code

--- demo.c
+++ demo.c
@@ -12,6 +12,40 @@ struct cex_status CEX;
 #define catch_all       cex_catch_all(GLOBAL_CEX)
 #define throw(T, value) cex_throw(GLOBAL_CEX, T, (value)) 
 
+// User-defined error callback function
+void error_callback( struct cex_status *st, enum cex_error err )
+{
+	const char *mesg;
+	switch ( err )
+	{
+		case CEX_NO_ERROR:
+			mesg = "CEX_NO_ERROR";
+			break;
+			
+		case CEX_ALLOC_ERROR:
+			mesg = "CEX_ALLOC_ERROR";
+			break;
+	
+		case CEX_STACK_OVERFLOW:
+			mesg = "CEX_STACK_OVERFLOW";
+			break;
+			
+		case CEX_STACK_UNDERFLOW:
+			mesg = "CEX_STACK_UNDERFLOW";
+			break;
+			
+		case CEX_UNCAUGHT:
+			mesg = "CEX_UNCAUGHT";
+			break;
+		
+		default:
+			mesg = "???";
+			break;
+	}
+	
+	fprintf( stderr, "CEX error: %s\n", mesg );
+	abort( );
+}
 
 int main( )
 {
@@ -19,34 +53,48 @@ int main( )
 	if ( cex_init_alloc( &CEX, 256 ) )
 		abort( );
 	
+	// Initialize error callback
+	CEX.error_callback = error_callback;
+	
 	try
 	{
 		printf( "in try{}\n" );
 		
-		// Something thrown here
-		throw( CEX_STRING, "'Exception description'" );
-		throw( CEX_INT, 17 );
-		
-		// Never reached
-		printf( "after throw\n" );
+		try
+		{
+			printf( "in try{try{}}\n" );
+			
+			// Throw things here
+			// throw( CEX_STRING, "'Exception description'" );
+			throw( CEX_INT, 17 );
+			// throw( CEX_VOID_PTR, NULL );
+			
+			printf( "never reached\n" );
+		}
+		catch
+		{	
+			printf( "in try{catch{}}\n" );
+			
+			// Handles only strings
+			catch_case( CEX_STRING )
+			{
+				// cex_what contains payload in catch cases
+				printf( "caught string: %s\n", cex_what );
+			}
+
+		}
 	}
 	catch
-	{	
-		// Handles only strings
-		catch_case( CEX_STRING )
-		{
-			// cex_what contains payload in catch cases
-			printf( "Caught string: %s\n", cex_what );
-		}
+	{
+		printf( "in catch{}\n" );
 		
-		// Handles all cases
-		catch_all
+		catch_case( CEX_INT )
 		{
-			printf( "Caught something\n" );
+			printf( "caught int: %d\n", cex_what );
 		}
 	}
-
+	
+	// Cleanup
 	cex_delete( &CEX );
-
 	return 0;
 }
